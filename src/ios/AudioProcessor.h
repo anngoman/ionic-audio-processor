2ba7639d5510a85ca903da3c40caa6b25af098e3
anngoman <ann.goman@gmail.com>
2016-06-28 13:38:45 +0300

    Add files via upload

---
+++ src/ios/AudioProcessor.h
@@ -0,0 +1,7 @@
+#import <Cordova/CDVPlugin.h>
+#import <objc/runtime.h>
+
+@interface AudioProcessor : CDVPlugin  {
+
+@end
+
---
+++ src/ios/AudioProcessor.m
@@ -0,0 +1,20 @@
+#import "AudioProcessor.h"
+#import <Cordova/CDVAvailability.h>
+
+@implementation AudioProcessor
+
+- (void) doIt:(CDVInvokedUrlCommand*)command {
+  CDVPluginResult* pluginResult = nil;
+  NSString* myarg = [command.arguments objectAtIndex:0];
+  
+  if (myarg != nil) {
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+  } else {
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
+  }
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
+@end
+
